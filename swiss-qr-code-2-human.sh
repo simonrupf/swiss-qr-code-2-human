9720534240800e12a11d669647670bb0d5d63d08
Simon Rupf <simon@rupf.net>
2021-04-10 05:27:48 +0200

    handle decoding errors, provide a clear error message with suggestions

--- swiss-qr-code-2-human.sh
+++ swiss-qr-code-2-human.sh
@@ -20,6 +20,7 @@ fi
 
 check_command zbarimg zbar-tools
 check_command awk gawk
+check_command awk gawk
 
 if [ ! -f "$1" ]
 then
@@ -27,4 +28,14 @@ then
     exit 2
 fi
 
-zbarimg -q "$1" | awk -f $(dirname $0)/swiss-qr-decode.awk
+if message=$(zbarimg -q "$1")
+then
+    echo "$message" | awk -f $(dirname $([ -L "$0" ] && readlink "$0" || echo "$0"))/swiss-qr-decode.awk
+else
+    echo >&2 "Failed to find or decode the QR code in image $1. Things to check:"
+    echo >&2 "- is the barcode large enough in the image?"
+    echo >&2 "- is the barcode mostly in focus?"
+    echo >&2 "- is there sufficient contrast/illumination?"
+    exit 4
+fi
+
